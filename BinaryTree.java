5e5a645aaab04d2398daff45a1b8eec8e45eaf74
SY <30416645+syong0710@users.noreply.github.com>
2022-09-03 09:35:57 -0700

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,11 +1,11 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class TreeNode{
     int value;
     TreeNode left;
     TreeNode right;
 
-    // The constructor
     public TreeNode(int nodeValue){
         value = nodeValue;
         left = null;
@@ -13,38 +13,86 @@ class TreeNode{
     }
 }
 
-public class BinaryTree{
-    public TreeNode root;
 
-    static List<Integer> results = new ArrayList<>();
-    public static List<Integer> inOrderTravList(TreeNode treeRoot){
-        if (treeRoot != null){
-            inOrderTravList(treeRoot.left);
-            // System.out.print(" " + treeRoot.value + " ");
-            results.add(treeRoot.value);
-            inOrderTravList(treeRoot.right);
+public class BinaryTree {
+    // Root of Binary Tree
+    TreeNode root;
+
+    // Constructors
+    BinaryTree(int rootValue) { root = new TreeNode(rootValue); }
+    BinaryTree() { root = null; }
+
+    // Print in-order traversal
+    void printInOrder(TreeNode root){
+        if(root == null){
+            return;
         }
+        printInOrder(root.left);
+        System.out.print(root.value + " ");
+        printInOrder(root.right);
+    }
+
+    // return a list, in-order traversal
+    public List<Integer> listInOrder(TreeNode root){
+        List<Integer> results = new ArrayList<>();
+        inOrderTrav(root,results);
         return results;
     }
 
+  void inOrderTrav(TreeNode root, List<Integer> resultList){
+        if(root == null){
+            return;
+        }
+        inOrderTrav(root.left, resultList);
+        resultList.add(root.value);
+        inOrderTrav(root.right,resultList);
+    }
+
+
+
     public static void main(String[] arg){
+
+        // The first Binary Tree
         BinaryTree tree1 = new BinaryTree();
+        // create root
         tree1.root = new TreeNode(1);
         tree1.root.left = new TreeNode(2);
         tree1.root.right = new TreeNode(3);
         tree1.root.left.left = new TreeNode(4);
         tree1.root.left.right = new TreeNode(5);
-
         /*
-
-                        1
-                     /    \
-                   2        3
-                 /   \
-                4     5
-
+                       1
+                    /    \
+                   2       3
+                 /  \
+                4    5
          */
 
-        System.out.println("In-order traversal:" + (inOrderTravList(tree1.root)) );
+        // The second Binary Tree
+        BinaryTree tree2 = new BinaryTree(11);
+        tree2.root.left = new TreeNode(13);
+        tree2.root.right = new TreeNode(15);
+        tree2.root.left.left = new TreeNode(17);
+        /*
+                        11
+                    /      \
+                   13       15
+                 /
+                17
+         */
+
+
+        System.out.println("in-order traversal of tree1: ");
+        tree1.printInOrder(tree1.root);
+        System.out.println(" ");
+
+        System.out.println("in-order traversal list of tree1: " + tree1.listInOrder(tree1.root));
+
+        System.out.println("in-order traversal of tree2: ");
+        tree2.printInOrder(tree2.root);
+        System.out.println(" ");
+
+        System.out.println("in-order traversal list of tree2: " + tree2.listInOrder(tree2.root));
+
     }
 }
